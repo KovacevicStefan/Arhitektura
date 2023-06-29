cb82b229894f0673bc73c3a0cf60785b6a57e394
KovacevicStefan <stefands00@hotmail.com>
2023-06-29 22:12:43 +0200

    arhitektura 29.06.

---
+++ Startup.cs
@@ -0,0 +1,57 @@
+using Arhitektura.IRepositories;
+using Arhitektura.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Arhitektura
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Registracija repozitorijuma kao servisa
+            services.AddScoped<IDestinacijaRepository, DestinacijaRepository>();
+            services.AddScoped<ITuristickaAgencijaRepository, TuristickaAgencijaRepository>();
+            services.AddScoped<IHotelRepository, HotelRepository>();
+            services.AddScoped<IAranzmanRepository, AranzmanRepository>();
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
